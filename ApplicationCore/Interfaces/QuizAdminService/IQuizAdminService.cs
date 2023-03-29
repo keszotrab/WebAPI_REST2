09c19b62a84b110a4082718469ef5740f5f66beb
bartosz.adamek <bartosz.adamek@BR-08>
2023-03-29 12:08:30 +0200

    Smutek i pożoga

--- ApplicationCore/Interfaces/QuizAdminService/IQuizAdminService.cs
+++ ApplicationCore/Interfaces/QuizAdminService/IQuizAdminService.cs
@@ -8,5 +8,5 @@ public interface IQuizAdminService
     public void UpdateQuizItem(int id, string question, List<string> incorrectAnswers, string correctAnswer, int points);
     public Quiz AddQuiz(string title, List<QuizItem> items);
     public List<QuizItem> FindAllQuizItems();
-    public List<Quiz> FindAllQuizzes();
+    public List<Quiz> FindAllQuizzes(); 
 }
